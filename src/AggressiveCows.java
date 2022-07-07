7515290c925e07f25668b8ee5229734f6f1ce124
Subrata Mandal <subratamandal@Subratas-MacBook-Air.local>
2022-07-07 17:51:11 +0530

    leanup

---
+++ src/AggressiveCows.java
@@ -0,0 +1,35 @@
+
+/**
+ * Farmer John has built a new long barn with N stalls. 
+ * Given an array of integers A of size N where each element of the array represents 
+ * the location of the stall and an integer B which represents the number of cows.
+ * His cows don't like this barn layout and become aggressive towards each other once 
+ * put into a stall. To prevent the cows from hurting each other, John wants to assign 
+ * the cows to the stalls, such that the minimum distance between any two of them is as 
+ * large as possible. What is the largest minimum distance?
+ * 
+ * Problem Constraints
+ * 2 <= N <= 100000
+ * 0 <= A[i] <= 109
+ * 2 <= B <= N
+ *
+ * The first argument given is the integer array A.
+ * The second argument given is the integer B.
+ *
+ * Input
+ * A = [1, 2, 3, 4, 5]
+ * B = 3
+ *
+ * Output: 2
+ * 
+ *  @author subratamandal
+ *  
+ */
+public class AggressiveCows {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
